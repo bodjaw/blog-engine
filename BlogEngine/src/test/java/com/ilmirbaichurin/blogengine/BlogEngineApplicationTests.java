e79f3520c1a595ab12053f84ac6a15a642e6f741
baichurin_ilmir <ilmir.baychurin@mail.ru>
2021-06-26 15:03:52 +0300

    project created

---
+++ BlogEngine/src/test/java/com/ilmirbaichurin/blogengine/BlogEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ilmirbaichurin.blogengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogEngineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
