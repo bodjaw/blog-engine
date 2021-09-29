4bb7dc89ca37e92ec2338edaab6f06be2ef1417b
Ilmir Baychurin <ilmir.baychurin@mail.ru>
2021-09-29 19:08:47 +0300

    dev branch

---
+++ src/main/java/com/ilmirbaichurin/blogengine/dao/api/response/dto/TagWithWeight.java
@@ -0,0 +1,14 @@
+package com.ilmirbaichurin.blogengine.dao.api.response.dto;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TagWithWeight {
+
+    private String name;
+    private double weight;
+
+}
